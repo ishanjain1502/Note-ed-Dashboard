11e998bd4210dcc0832ac315f317a71c307480b6
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2022-04-09 01:28:45 +0530

    Add : Login Auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 import {Route,Routes} from 'react-router-dom';
 import VideoList from './Components/VideoList';
 import VideoPage from './Components/VideoPage';
+import Login from "./Components/Auth/Login";
+import Register from './Components/Auth/Register';
 function App() {
+  const [loggedInStatus,setloggedInStatus] = useState(true)
   return (
     <Routes>
       <Route path='/dashboard' element={<VideoList/>}></Route>
+      <Route path='/login' element={<Login loggedInStatus={loggedInStatus} setloggedInStatus={setloggedInStatus}/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
       <Route path='/video/:videoname' element={<VideoPage/>}></Route>
-    
     </Routes>
   );
 }
--- src/Components/VideoPage.jsx
+++ src/Components/VideoPage.jsx
@@ -1,88 +1,83 @@
-import React,{useEffect,useState,useRef} from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios';
-import { useParams,useLocation } from 'react-router-dom'
+import { useParams, useLocation } from 'react-router-dom'
 import Header from './Header';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ReactPlayer from 'react-player/youtube'
+
 export default function VideoPage(props) {
-    let {videoname}=useParams();
-    let {state}=useLocation();
-    let {video_url,video_id}=state;
-useEffect(()=>{
-    fetchNotes();
-},[])
 
+    let { videoname } = useParams();
+    let { state } = useLocation();
+    let { video_url, video_id } = state;
+    useEffect(() => {
+        fetchNotes();
+        //eslint-disable-next-line
+    }, [])
 
-const player=useRef();
+    const player = useRef();
+    const [timestamp, setTimstamp] = useState();
+    const fetchNotes = (name) => {
+        axios.get(`https://Backend-1.prathameshdukare.repl.co/api/v1/video/${videoname}`, {
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNpeWEiLCJlbWFpbCI6InNpeWFAZ21haWwuY29tIiwidXNlcl9pZCI6IjYyMzczMDhkZTlmZTZiNmJhYjYxOTU1NiIsImlhdCI6MTY0ODgyNTIyNX0.Eylls1_gGvXmuU8IrI_nTr7VZZWb2Qp4TarfCcF4ulY'
+            }
+        }).then(data => {
+            console.log(data);
+            setTimstamp(data.data.data);
 
+        })
+    }
 
-const [timestamp,setTimstamp]=useState();
+    const convertToseconds = (time) => {
+        const timeArr = time.split(":");
+        let sec = 0;
+        let pow = 2;
+        timeArr.forEach(time => {
+            sec += (time * Math.pow(60, pow));
+            pow--;
+        });
+        console.log(sec);
+        return sec;
+    }
 
+    return (
 
-const fetchNotes=(name)=>{
-    axios.get(`https://Backend-1.prathameshdukare.repl.co/api/v1/video/${videoname}`,{
-        headers:{
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNpeWEiLCJlbWFpbCI6InNpeWFAZ21haWwuY29tIiwidXNlcl9pZCI6IjYyMzczMDhkZTlmZTZiNmJhYjYxOTU1NiIsImlhdCI6MTY0ODgyNTIyNX0.Eylls1_gGvXmuU8IrI_nTr7VZZWb2Qp4TarfCcF4ulY'
-        }
-    }).then(data=>{
-        console.log(data);
-        setTimstamp(data.data.data);
-        
-    })
-}
+        <>
 
+            <Header></Header>
+            <div className='mt-12 mb-28 mx-64  rounded flex flex-col content-center bg-indigo-300 pt-8'>
 
-const convertToseconds=(time)=>{
-    const timeArr=time.split(":");
-    let sec=0;
-    let pow=2;
-    timeArr.forEach(time => {
-        sec+=(time*Math.pow(60,pow));
-        pow--;
-    });
-    console.log(sec);
-    return sec;
-}
+                <ReactPlayer className='self-center' ref={player} controls={true} url={video_url} />
+                <div className='self-start w-full px-32 mt-10'>
+                    {timestamp && timestamp.map(timestamp => {
+                        const time = Object.keys(timestamp);
+                        const seconds = convertToseconds(time[0]);
+                        return <div className='mb-6'>
+                            <div className='flex'>
 
+                                <p className='bg-indigo-700 mr-2 w-32 mb-2 rounded cursor-pointer flex justify-center' onClick={() => {
+                                    player.current.seekTo(seconds);
+                                }}>{time}</p>
+                                <ModeEditIcon className='mr-2' />
+                                <DeleteIcon />
+                            </div>
+                            <div className='w-11/12 ml-1 flex'>
+                                <ModeCommentIcon />
+                                <p className='ml-2'>{timestamp[time]}</p>
+                            </div>
 
-    
-  return (
-    
-    <>
-      
-       <Header></Header>
-       <div className='mt-12 mb-28 mx-64  rounded flex flex-col content-center bg-indigo-300 pt-8'>
-
-        <ReactPlayer className='self-center' ref={player} controls={true} url={video_url} />
-            <div className='self-start w-full px-32 mt-10'>
-                {timestamp && timestamp.map(timestamp=>{
-                    const time=Object.keys(timestamp);
-                    const seconds=convertToseconds(time[0]);
-                    return <div className='mb-6'>
-                        <div className='flex'>
-
-                        <p className='bg-indigo-700 mr-2 w-32 mb-2 rounded cursor-pointer flex justify-center' onClick={()=>{
-                           player.current.seekTo(seconds);
-                       }}>{time}</p>
-                       <ModeEditIcon className='mr-2'/>
-                       <DeleteIcon/>
                         </div>
-                       <div className='w-11/12 ml-1 flex'>
-                        <ModeCommentIcon/>
-                        <p className='ml-2'>{timestamp[time]}</p>
-                       </div>
-                       
-                    </div>
-                })}
+                    })}
 
+                </div>
             </div>
-       </div>
-      </> 
+        </>
 
 
-  )
+    )
 
-  
+
 }
