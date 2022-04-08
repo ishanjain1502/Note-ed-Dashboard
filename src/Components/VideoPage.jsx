40a7e17e2eae2c22de5a55791ce46138ed64cd87
Shofiya Bootwala <shofiyabootwala@gmail.com>
2022-04-08 12:37:26 +0530

    added delete and edit icons

--- src/Components/VideoPage.jsx
+++ src/Components/VideoPage.jsx
@@ -2,6 +2,9 @@ import React,{useEffect,useState,useRef} from 'react'
 import axios from 'axios';
 import { useParams,useLocation } from 'react-router-dom'
 import Header from './Header';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import ReactPlayer from 'react-player/youtube'
 export default function VideoPage(props) {
     let {videoname}=useParams();
@@ -50,19 +53,27 @@ const convertToseconds=(time)=>{
     <>
       
        <Header></Header>
-       <div className='mt-12 mb-28 mx-64 rounded flex flex-col content-center bg-indigo-300 pt-8'>
+       <div className='mt-12 mb-28 mx-64  rounded flex flex-col content-center bg-indigo-300 pt-8'>
 
         <ReactPlayer className='self-center' ref={player} controls={true} url={video_url} />
-            <div className='self-start px-32 mt-10'>
+            <div className='self-start w-full px-32 mt-10'>
                 {timestamp && timestamp.map(timestamp=>{
                     const time=Object.keys(timestamp);
                     const seconds=convertToseconds(time[0]);
                     return <div className='mb-6'>
-                        
-                        <p className='bg-indigo-700 mb-2 rounded cursor-pointer flex justify-center' onClick={()=>{
+                        <div className='flex'>
+
+                        <p className='bg-indigo-700 mr-2 w-32 mb-2 rounded cursor-pointer flex justify-center' onClick={()=>{
                            player.current.seekTo(seconds);
                        }}>{time}</p>
-                       <p>{timestamp[time]}</p>
+                       <ModeEditIcon className='mr-2'/>
+                       <DeleteIcon/>
+                        </div>
+                       <div className='w-11/12 ml-1 flex'>
+                        <ModeCommentIcon/>
+                        <p className='ml-2'>{timestamp[time]}</p>
+                       </div>
+                       
                     </div>
                 })}
 
