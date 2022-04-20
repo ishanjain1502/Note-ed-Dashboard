857e20c919a7f76bbc1571e323ae87803f9d7bc2
Shofiya2003 <shofiyabootwala@gmail.com>
2022-04-20 12:24:35 +0530

    added token to localstorage

--- src/Components/dashboard/videos/VideoHome.jsx
+++ src/Components/dashboard/videos/VideoHome.jsx
@@ -2,17 +2,15 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Video from './Video';
 
-const siyasToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlNpeWEiLCJlbWFpbCI6InNpeWFAZ21haWwuY29tIiwidXNlcl9pZCI6IjYyMzczMDhkZTlmZTZiNmJhYjYxOTU1NiIsImlhdCI6MTY0ODgyNTIyNX0.Eylls1_gGvXmuU8IrI_nTr7VZZWb2Qp4TarfCcF4ulY"
-const ramsToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlJhbSIsImVtYWlsIjoicmFtQGdtYWlsLmNvbSIsInVzZXJfaWQiOiI2MjU1M2U0MzgzMWEyYjE4N2IyZWEyZDciLCJpYXQiOjE2NDk3NTM2Njd9.jcOqw_X7Ve7KyL3SZcfVWQN4xDfYeOFE4KSbep1P3f0"
-
 export default function VideoHome() {
     const [videos, setVideos] = useState();
-    let host = "https://Backend-1.prathameshdukare.repl.co"
+    let host = "http://localhost:8000"
 
     useEffect(() => {
-        axios.get(`${host}/api/v1/videos`, {
+        const token=localStorage.getItem('token').toString();
+        axios.get(`${host}/api/v1/videos/?deleted=false`, {
             headers: {
-                Authorization: `Bearer ${ramsToken}`
+                Authorization: `Bearer ${token}`
             }
         }).then(data => {
             console.log(data);
