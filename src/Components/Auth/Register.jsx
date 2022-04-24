7435bdb33abe79b05f3d9243b46b7555bc331082
RavjotSandhu <nand2072@gmail.com>
2022-04-24 16:14:31 +0530

    revamp register page UI

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import HomeHeader from '../home/HomeHeader';
+
 
 export default function Register() {
 
@@ -42,74 +42,85 @@ export default function Register() {
     }
 
     return (
-        <>
-        <HomeHeader/>
-
         <div>
-            <div className="bg-grey-lighter min-h-screen flex flex-col">
-                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                                <p className='text-4xl mb-8 italic font-semibold' >Register</p>
-                    <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full shadow-slate-700 ">
-                        
-                        <input
-                            type="text"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="username"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="User Name" />
+            <nav>
+                <div style={{
+                    "fontSize": "50px",
+                    "fontWeight": "700",
+                    "marginLeft": "9vw",
+                    "marginTop": "0rem",
+                    "paddingTop": "1rem",
+                    "marginBottom": "-2rem",
+                    "color": "#0091ad",
+                }}><a href="/">Noted</a></div>
+            </nav>
+            <div className='register-page'>
 
-                        <input
-                            type="text"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Email" />
+                <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                    <div className="max-w-md w-full space-y-8">
+                        <div>
+                            <h2 className="font-custom mt-6 text-center text-3xl font-extrabold text-gray-900">Register on Noted</h2>
 
-                        <input
-                            type="password"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)} />
-                        <input
-                            type="password"
-                            class="block border border-grey-light w-full p-3 rounded mb-4"
-                            name="confirm_password"
-                            placeholder="Confirm Password"
-                            value={comfirm}
-                            onChange={(e) => setComfirm(e.target.value)} />
-                        <div >
-                        <button
-                            type="submit"
-                            class="w-full text-center py-3 rounded bg-green text-white bg-blue-600 hover:bg-blue-500 focus:outline-none my-1"
-                            value="Register"
-                            onClick={registerUser}
-                        >Create Account</button>
-                        <ToastContainer/>
                         </div>
-                        <div class="text-center text-sm text-grey-dark mt-4">
-                            By signing up, you agree to the
-                            <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                                Terms of Service
-                            </a> and
-                            <a class="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                                Privacy Policy
-                            </a>
-                        </div>
-                    </div>
+                        <form className="mt-8 space-y-6" action="#" method="POST">
+                            <input type="hidden" name="remember" defaultValue="true" />
+                            <div className="rounded-md shadow-sm -space-y-px">
+                            <input
+                                type="text"
+                                class="font-custom appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-custom focus:border-custom focus:z-10 sm:text-sm"
+                                name="username"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                placeholder="User Name" />
 
-                    <div class="text-grey-dark mt-6">
-                        Already have an account?
-                        <a class="no-underline border-b border-blue text-blue" href="../login/">
-                            Log in
-                        </a>.
+                            <input
+                                type="text"
+                                class="font-custom appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-none focus:outline-none focus:ring-custom focus:border-custom focus:z-10 sm:text-sm"
+                                name="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Email" />
+
+                            <input
+                                type="password"
+                                class="font-custom appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-none focus:outline-none focus:ring-custom focus:border-custom focus:z-10 sm:text-sm"
+                                name="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
+                            <input
+                                type="password"
+                                class="font-custom appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-custom focus:border-custom focus:z-10 sm:text-sm"
+                                name="confirm_password"
+                                placeholder="Confirm Password"
+                                value={comfirm}
+                                onChange={(e) => setComfirm(e.target.value)} />
+                                    
+                            </div>
+
+                            <div>
+                                <button
+                                    type="submit"
+                                    onClick={registerUser}
+                                    className="font-custom group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-custom hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom"
+                                >
+                                    <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                        {/* <LockClosedIcon className="h-5 w-5 text-custom group-hover:text-indigo-400" aria-hidden="true" /> */}
+                                    </span>
+                                    Register
+                                </button>
+                            </div>
+                            <div class="font-custom text-center text-grey-dark mt-6">
+                                Already have an account?
+                                <a class="font-custom border-b-4 border-blue text-underline" href="../login/">
+                                    Log in
+                                </a>
+                            </div>
+                        </form>
                     </div>
                 </div>
+
             </div>
         </div>
-        </>
     )
 }
